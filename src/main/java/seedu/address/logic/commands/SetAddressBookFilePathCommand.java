230636b2360835fa3d70cd7c1b1fb3f69ff80863
davinkhor <73937826+deltaMinor@users.noreply.github.com>
2026-03-21 13:08:54 +0800

    feat: Move storage file path

---
+++ src/main/java/seedu/address/logic/commands/SetAddressBookFilePathCommand.java
@@ -0,0 +1,41 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.commons.util.AppUtil.checkArgument;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.nio.file.Path;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Changes the address book file path.
+ */
+public class SetAddressBookFilePathCommand extends SetCommand {
+    public static final String MESSAGE_SUCCESS = "Set data source path to %1$s";
+    public static final String MESSAGE_CONSTRAINTS =
+            "File names should only contain alphanumeric characters and the underscore character '_'.";
+
+    private final String fileName;
+
+    /**
+     * @param fileName Name of the address book file to change to.
+     */
+    public SetAddressBookFilePathCommand(String fileName) {
+        requireAllNonNull(fileName);
+        checkArgument(UserPrefs.isValidFileName(fileName), MESSAGE_CONSTRAINTS);
+        this.fileName = fileName;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        Path filePath = UserPrefs.formatAddressBookFilePath(fileName);
+        model.setAddressBookFilePath(filePath);
+        model.resetDisplayedContactList();
+
+        String feedback = String.format(MESSAGE_SUCCESS, filePath);
+        model.saveSnapshot(feedback);
+        return new CommandResult(feedback);
+    }
+}
