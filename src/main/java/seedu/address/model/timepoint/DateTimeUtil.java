9f789c93b43d94bbdc6320b5865bc8ef38727c45
Cxyu629 <chowxingyu123@gmail.com>
2026-03-24 11:48:27 +0800

    docs: add docs

--- src/main/java/seedu/address/model/timepoint/DateTimeUtil.java
+++ src/main/java/seedu/address/model/timepoint/DateTimeUtil.java
@@ -4,7 +4,16 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+/**
+ * Utility class for {@link LocalDateTime} formatting.
+ */
 public class DateTimeUtil {
+    /**
+     * Formats a {@link LocalDateTime} into a displayable string.
+     *
+     * @param ldt The {@link LocalDateTime} to format.
+     * @return The formatted string.
+     */
     public static String toDisplayString(LocalDateTime ldt) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm, MMM d, yyyy", Locale.ROOT);
         return ldt.format(formatter);
