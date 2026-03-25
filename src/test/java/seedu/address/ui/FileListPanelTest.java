524b52643f35893cac890f8a73cd3c561cd64037
davinkhor <73937826+deltaMinor@users.noreply.github.com>
2026-03-25 14:56:46 +0800

    test: Add FileListPanel construct test

--- src/test/java/seedu/address/ui/FileListPanelTest.java
+++ src/test/java/seedu/address/ui/FileListPanelTest.java
@@ -2,15 +2,21 @@ package seedu.address.ui;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.exceptions.DataLoadingException;
+import seedu.address.commons.util.FileUtil;
+import seedu.address.commons.util.JsonUtil;
 
 public class FileListPanelTest extends GuiUnitTest {
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableAddressBookTest");
@@ -36,6 +42,15 @@ public class FileListPanelTest extends GuiUnitTest {
     public void constructTest() throws Exception {
         runAndWait(() -> {
             assertDoesNotThrow(() -> new FileListPanel(TEST_DATA_FOLDER));
+            Path temporaryFilePath = TEST_DATA_FOLDER.resolve("temp.json");
+            try {
+                Files.createFile(temporaryFilePath);
+                assertTrue(Files.exists(temporaryFilePath));
+                Files.write(temporaryFilePath, Files.readAllBytes(TYPICAL_CONTACTS_FILE));
+                Files.delete(temporaryFilePath);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         });
     }
 }
