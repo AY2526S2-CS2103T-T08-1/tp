f6082f4f973b7316e022b5ae65971bdd3b8ab075
davinkhor <73937826+deltaMinor@users.noreply.github.com>
2026-03-30 18:57:52 +0800

    Change temp

---
+++ src/main/java/seedu/address/logic/commands/FileCommand.java
@@ -0,0 +1,27 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CLEAR_ALL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CLEAR_LINE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CLEAR_OLDEST;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EDIT_LINE;
+
+public abstract class FileCommand {
+    public static final String COMMAND_WORD = "file";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Manages the contact list files being used. "
+            + "Format [Add]: " + COMMAND_WORD + " INDEX NOTE\n"
+            + "Format [Edit line]: " + COMMAND_WORD + " INDEX " + PREFIX_EDIT_LINE + "NOTE_INDEX NEW_NOTE\n"
+            + "Format [Clear line]: " + COMMAND_WORD + " INDEX " + PREFIX_CLEAR_LINE + "NOTE_INDEX\n"
+            + "Format [Clear oldest]: " + COMMAND_WORD + " INDEX " + PREFIX_CLEAR_OLDEST + "COUNT\n"
+            + "Format [Clear all]: " + COMMAND_WORD + " INDEX " + PREFIX_CLEAR_ALL + "\n"
+            + "Parameters:\n"
+            + "- INDEX (must be a positive integer)\n"
+            + "- NOTE (should be a non-empty string)\n"
+            + "- NOTE_INDEX (must be a positive integer)\n"
+            + "- COUNT (must be a non-negative integer)\n"
+            + "Example [Add]: " + COMMAND_WORD + " 1 " + "Likes to swim.\n"
+            + "Example [Edit line]: " + COMMAND_WORD + " 1 " + PREFIX_EDIT_LINE + "1 Updated note text.\n"
+            + "Example [Clear line]: " + COMMAND_WORD + " 1 " + PREFIX_CLEAR_LINE + "2\n"
+            + "Example [Clear oldest]: " + COMMAND_WORD + " 1 " + PREFIX_CLEAR_OLDEST + "1\n"
+            + "Example [Clear all]: " + COMMAND_WORD + " 1 " + PREFIX_CLEAR_ALL + "\n";
+}
